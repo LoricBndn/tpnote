67bac2f82fbe1ee3e4f88a8c6b8bb98cd65635c5
Loric <loric.bondon@etu.univ-lorraine.fr>
2024-11-01 17:42:53 +0100

    Fichier prodByFact.class.php

---
+++ modele/prodByFact.class.php
@@ -0,0 +1,39 @@
+<?php
+    require_once 'produit.class.php';
+
+class EquiptBySalle {
+    // table ligne
+
+    private $numFact;
+    private $produit; 
+    private $qteProd;
+
+    function __construct(string $numFact = '', Produit $produit = null, int $qteProd = 0){
+        $this->numFact = $numFact;
+        $this->produit = $produit;
+        $this->qteProd = $qteProd;
+    }
+
+     // getters
+    function getNumFact(): string {
+        return $this->numFact;
+    }
+    function getProduit(): Produit {
+        return $this->produit;
+    }
+    function getQteProd(): int {
+        return $this->qteProd;
+    }
+
+    // setters    
+    function setNumFact(string $numFact):void{
+        $this->numFact = $numFact;
+    }
+    function setProduit(Produit $produit):void{
+        $this->produit = $produit;
+    }
+    function setQteProd(int $qteProd):void{
+        $this->qteProd = $qteProd;
+    }
+}
+?>
